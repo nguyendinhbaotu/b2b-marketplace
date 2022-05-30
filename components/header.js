af062b1e96e1e6ec8f0fc8a44310fb8c1b99420e
Tu Nguyen <tu.nguyen@catelasinc.onmicrosoft.com>
2022-05-30 18:51:07 +0700

    Temperary hide hot links

--- components/header.js
+++ components/header.js
@@ -7,10 +7,10 @@ export default function Header() {
                 <div class="inline-block">
                     <img src={`logo_updated.png`} width="100px"/>
                 </div>
-                <div class="inline-block"><Button label="HOT SERVICES" className="p-button-text" /></div>
-                <div class="inline-block"><Button label="REQUEST QUOTES" className="p-button-text" /></div>
-                <div class="inline-block"><Button label="LOGISTICS COMPANIES" className="p-button-text" /></div>
-                <div class="inline-block"><Button label="LOGISTICS MARKET" className="p-button-text" /></div>
+                {/* <div class="inline-block"><Button label="HOT SERVICES" className="p-button-text" /></div> */}
+                {/* <div class="inline-block"><Button label="REQUEST QUOTES" className="p-button-text" /></div> */}
+                {/* <div class="inline-block"><Button label="LOGISTICS COMPANIES" className="p-button-text" /></div> */}
+                {/* <div class="inline-block"><Button label="LOGISTICS MARKET" className="p-button-text" /></div> */}
                 <div class="ml-auto">
                     <Button label="REGISTER" className="p-button-outlined" />
                     <Button label="SIGN IN" className="ml-1 p-button-rounded" />
