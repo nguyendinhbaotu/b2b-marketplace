76498733fc365e1d777f4eeab498dc5dbe65a1ea
Tu Nguyen <tu.nguyen@catelasinc.onmicrosoft.com>
2022-05-30 12:37:27 +0700

    Fix yarn build, add eslint

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { classNames } from 'primereact/utils';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { getProducts } from './ProductService';
+import { getProducts } from './api/ProductService';
 import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { Rating } from 'primereact/rating';
